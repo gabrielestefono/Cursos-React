a6a5eb4f90c5813fee9656e4651f19010c1a6f46
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-03 10:32:12 -0300

    Importação de componentes

--- mateus_battisti/fundamentos/src/App.jsx
+++ mateus_battisti/fundamentos/src/App.jsx
@@ -1,13 +1,16 @@
+// Components
 import FirstComponent from './components/FirstComponent'
+
+// Styles / CSS
 import './App.css'
 
 
 function App() {
-
   return (
-    <>
+    <div>
+      <h1>Fundamentos React</h1>
       <FirstComponent/>
-    </>
+    </div>
   )
 }
 
