a9a697c373808db924d2ec3013f3e9ed6ed96cb1
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-03 13:54:40 -0300

    MB - Fundamentos - Função de Renderização

--- mateus_battisti/fundamentos/src/components/Events.jsx
+++ mateus_battisti/fundamentos/src/components/Events.jsx
@@ -1,5 +1,15 @@
 const Events = () => {
 	const handleMyEvent = () => console.log('Ativou o evento!');
+	const renderSomething = (x) => {
+		if (x) {
+			return (
+				<h1>Renderizando isso!</h1>
+			)
+		} else {
+			return <h1>Também posso renderizar isso!</h1>
+		}
+	};
+
 	return (
 		<div>
 			<div>
@@ -14,6 +24,8 @@ const Events = () => {
 					}
 				}}>Clique aqui também</button>
 			</div>
+			<div>{renderSomething(true)}</div>
+			<div>{ renderSomething(false) }</div>
 		</div>
 	)
  }
