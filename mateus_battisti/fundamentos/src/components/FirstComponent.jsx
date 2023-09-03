4e19309ad5df783077f82d6d7fdef0046cb37008
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-03 10:57:44 -0300

    MB - Fundamentos - Comentários

--- mateus_battisti/fundamentos/src/components/FirstComponent.jsx
+++ mateus_battisti/fundamentos/src/components/FirstComponent.jsx
@@ -1,6 +1,10 @@
+// Arquivos de estilo
+
 const FirstComponent = () => {
+	// Essa função cria o primeiro componente
 	return (
 		<div>
+			{/* Algum comentário */}
 			<h1>Meu primeiro componente</h1>
 			<p className="teste">Meu texto</p>
 		</div>
