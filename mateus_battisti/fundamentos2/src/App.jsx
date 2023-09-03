1410b82a6b3b5433dc97edffd7da12b7ba87b643
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-03 17:44:39 -0300

    MB - Fundamentos02 - Inserindo imagens via public

--- mateus_battisti/fundamentos2/src/App.jsx
+++ mateus_battisti/fundamentos2/src/App.jsx
@@ -5,6 +5,10 @@ function App() {
   return (
     <>
       <h1>Avançando no react</h1>
+      {/* Imagem em public */}
+      <div>
+        <img src="./working.jpg" alt="Man working showing a clock, a pen and a notebook" width={600} height={400}/>
+      </div>
     </>
   )
 }
