83da41b03b03d79657b27746b1d83b9f4a1a069e
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-07 22:25:45 -0300

    MB - CSS - CSS Inline Dinâmico

--- mateus_battisti/tipos_css/src/App.jsx
+++ mateus_battisti/tipos_css/src/App.jsx
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import './App.css'
 import MyComponent from './components/MyComponent'
 
 function App() {
-
+  const n = 15;
+  const [name] = useState("Gabriel");
   return (
     <div>
       <h1>React com CSS</h1>
       <MyComponent />
       <p>Verifique que assim o estilo &apos;vaza&apos;, este parágrafo é do App.jsx</p>
-      <p style={{color: "blue", backgroundColor: "yellow"}}>Este elemento foi estilizado de forma inline</p>
+      <p style={{ color: "blue", backgroundColor: "yellow" }}>Este elemento foi estilizado de forma inline</p>
+      <h2 style={n < 10 ? { backgroundColor: "pink" } : { backgroundColor: "purple" }}>Esse é o título colorido</h2>
+      <h2 style={name == "Gabriel" ? { color: "green", backgroundColor: "black" } : null}>{name}</h2>
     </div>
   )
 }
