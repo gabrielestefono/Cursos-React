bf1feeade25b147289a9f1e8860b7c269f7b154a
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-08 14:10:38 -0300

    MB - Formulários - Label envonvendo input

--- mateus_battisti/forms/src/components/MyForm.jsx
+++ mateus_battisti/forms/src/components/MyForm.jsx
@@ -8,6 +8,10 @@ const MyForm = () => {
 					<label htmlFor="name">Nome</label>
 					<input type="text" name="name" placeholder="Digite o seu nome"/>
 				</div>
+				<label>
+					<span>Email</span>
+					<input type="text" name="Email" placeholder="Digite o seu email"/>
+				</label>
 				<input type="submit" value="Enviar"/>
 			</form>
 		</>
