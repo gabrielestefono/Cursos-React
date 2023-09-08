15d31f112bed2ca90532b17d44f0962156ccf66a
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-07 22:14:23 -0300

    MB - CSS - CSS inline

--- mateus_battisti/tipos_css/src/App.jsx
+++ mateus_battisti/tipos_css/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
       <h1>React com CSS</h1>
       <MyComponent />
       <p>Verifique que assim o estilo &apos;vaza&apos;, este parágrafo é do App.jsx</p>
+      <p style={{color: "blue", backgroundColor: "yellow"}}>Este elemento foi estilizado de forma inline</p>
     </div>
   )
 }
