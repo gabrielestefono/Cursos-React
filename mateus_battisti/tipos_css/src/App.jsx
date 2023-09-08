62c34649ef8ae9e1b76266461c62b9bc120ca520
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-07 22:36:06 -0300

    MB - CSS - Classes Dinâmicas no CSS

--- mateus_battisti/tipos_css/src/App.jsx
+++ mateus_battisti/tipos_css/src/App.jsx
@@ -5,6 +5,7 @@ import MyComponent from './components/MyComponent'
 function App() {
   const n = 15;
   const [name] = useState("Gabriel");
+  const redTitle = true;
   return (
     <div>
       <h1>React com CSS</h1>
@@ -13,6 +14,7 @@ function App() {
       <p style={{ color: "blue", backgroundColor: "yellow" }}>Este elemento foi estilizado de forma inline</p>
       <h2 style={n < 10 ? { backgroundColor: "pink" } : { backgroundColor: "purple" }}>Esse é o título colorido</h2>
       <h2 style={name == "Gabriel" ? { color: "green", backgroundColor: "black" } : null}>{name}</h2>
+      <h2 className={redTitle ? "red_title" : "title"}>Este título vai ter a classe dinâmica</h2>
     </div>
   )
 }
