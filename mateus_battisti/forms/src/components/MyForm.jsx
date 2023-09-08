b73b9c3a41a00d8dae614bae8ce567f96a60ccae
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-08 13:59:10 -0300

    MB - Formulários - Criando Formulários

---
+++ mateus_battisti/forms/src/components/MyForm.jsx
@@ -0,0 +1,16 @@
+import "./MyForm.css";
+
+const MyForm = () => {
+	return (
+		<>
+			<form>
+				<div>
+					<label htmlFor="name">Nome</label>
+					<input type="text" name="name" placeholder="Digite o seu nome"/>
+				</div>
+				<input type="submit" value="Enviar"/>
+			</form>
+		</>
+	)
+}
+export default MyForm;
