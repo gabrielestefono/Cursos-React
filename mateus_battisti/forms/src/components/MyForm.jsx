72f96da2ad8179f43de8f470faa0fd6924e8ce97
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-08 14:27:16 -0300

    MB - Formulários - Gerenciamento dados input

--- mateus_battisti/forms/src/components/MyForm.jsx
+++ mateus_battisti/forms/src/components/MyForm.jsx
@@ -1,16 +1,29 @@
+import { useState } from "react";
 import "./MyForm.css";
 
 const MyForm = () => {
+
+	const [name, setName] = useState();
+	const [email, SetEmail] = useState();
+
+	const handleName = (e) => {
+		setName(e.target.value)
+	}
+
+	const handleEmail = (e) => {
+		SetEmail(e.target.value)
+	}
+
 	return (
 		<>
 			<form>
 				<div>
 					<label htmlFor="name">Nome</label>
-					<input type="text" name="name" placeholder="Digite o seu nome"/>
+					<input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName}/>
 				</div>
 				<label>
 					<span>Email</span>
-					<input type="text" name="Email" placeholder="Digite o seu email"/>
+					<input type="text" name="Email" placeholder="Digite o seu email" onChange={handleEmail}/>
 				</label>
 				<input type="submit" value="Enviar"/>
 			</form>
