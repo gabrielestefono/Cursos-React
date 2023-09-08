1fa4e80f23caea407017c5d1464ad47cb89da225
gabrielestefono <gabrielestefono@hotmail.com>
2023-09-07 22:13:28 -0300

    MB - CSS - CSS de Componentes

--- mateus_battisti/tipos_css/src/App.jsx
+++ mateus_battisti/tipos_css/src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import MyComponent from './components/MyComponent'
 
 function App() {
 
   return (
     <div>
       <h1>React com CSS</h1>
+      <MyComponent />
+      <p>Verifique que assim o estilo &apos;vaza&apos;, este parágrafo é do App.jsx</p>
     </div>
   )
 }
