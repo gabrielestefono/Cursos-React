87d0840828c2190e545e6c9ac979ae398b042e23
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-08 07:47:01 -0300

    Resgatando dados da API

--- mateus_battisti/07_requisicoes_http_react/src/App.tsx
+++ mateus_battisti/07_requisicoes_http_react/src/App.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect }from 'react';
 import './App.css';
+import { ProductInterface } from './interfaces/product.interface';
 
 function App() {
+  // Resgatando dados
+  const url = "http://localhost:3000/products";
+
+  const [products, setProducts] = useState<ProductInterface[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch(url);
+      const data: ProductInterface[] = await res.json();
+      setProducts(data);
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(products);
+  
   return (
     <div className="App">
       <h1>Lista de Produtos</h1>
+      <ul>
+        {products.map((produto)=>(<li key={produto.id}>{produto.name} - R$ {produto.price}</li>))}
+      </ul>
     </div>
   );
 }
