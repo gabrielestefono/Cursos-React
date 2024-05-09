71a7c6bf017431f1128c1c02bc4f1cc63e9f5b90
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-09 09:14:43 -0300

    Link Ativo

--- mateus_battisti/08_react_router/src/components/navbar.tsx
+++ mateus_battisti/08_react_router/src/components/navbar.tsx
@@ -1,11 +1,11 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import './navbar.css';
 
 export default function Navbar(){
 	return(
 		<nav>
-			<Link to="/">Home</Link>
-			<Link to="/about">About</Link>
+			<NavLink to="/">Home</NavLink>
+			<NavLink to="/about">About</NavLink>
 		</nav>
 	)
 }
