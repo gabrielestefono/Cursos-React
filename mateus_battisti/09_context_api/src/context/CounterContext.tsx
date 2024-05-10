50855d59db02b48a26b996f4bfb09c33df5507a3
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-10 15:55:18 -0300

    Alterando Contexto

--- mateus_battisti/09_context_api/src/context/CounterContext.tsx
+++ mateus_battisti/09_context_api/src/context/CounterContext.tsx
@@ -3,7 +3,7 @@ import React, { createContext, useState } from 'react';
 export const CounterContext = createContext({});
 
 export const CounterContextProvider = ({children}: any) => {
-	const [counter, setCounter] = useState<number>(5);
+	const [counter, setCounter] = useState<number>(0);
 
 	return (
 		<CounterContext.Provider value={{counter, setCounter}}>
