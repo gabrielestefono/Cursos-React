7aec664623a00e0c10f0ee054baedcb69d348886
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-13 12:59:36 -0300

    Passando Usuário para o contexto

--- mateus_battisti/10_mini_blog/src/App.tsx
+++ mateus_battisti/10_mini_blog/src/App.tsx
@@ -1,6 +1,8 @@
 import './App.css';
 
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { onAuthStateChanged } from 'firebase/auth';
+import useAuthentication from './hooks/useAuthentication';
 
 // Pages
 import Home from './pages/Home/Home';
@@ -9,24 +11,42 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer.module';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
-
-
+import { AuthProvider } from './context/AuthContext';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [user, setUser] = useState<any>(undefined);
+  const { auth } = useAuthentication();
+
+  const loadingUser = user === undefined;
+
+  useEffect(()=>{
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+    });
+  },[auth])
+
+  if(loadingUser){
+    return <p>Carregando...</p>
+  }
+
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar/>
-        <div className="container">
-          <Routes>
-          <Route path='/' element={<Home/>}/>
-            <Route path='/about' element={<About/>}/>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/register' element={<Register/>}/>
-          </Routes>
-        </div>
-        <Footer/>
-      </BrowserRouter>
+      <AuthProvider value={user}>
+        <BrowserRouter>
+          <Navbar/>
+          <div className="container">
+            <Routes>
+            <Route path='/' element={<Home/>}/>
+              <Route path='/about' element={<About/>}/>
+              <Route path='/login' element={<Login/>}/>
+              <Route path='/register' element={<Register/>}/>
+            </Routes>
+          </div>
+          <Footer/>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
