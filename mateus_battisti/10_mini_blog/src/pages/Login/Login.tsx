4c8a3409dc5518c768e7e456985b1acfd71d5d52
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-13 13:25:47 -0300

    Estruturando página de login

--- mateus_battisti/10_mini_blog/src/pages/Login/Login.tsx
+++ mateus_battisti/10_mini_blog/src/pages/Login/Login.tsx
@@ -1,11 +1,42 @@
+import React, { useState } from 'react';
+import useAuthentication from '../../hooks/useAuthentication';
 import styles from './Login.module.css';
 
 export default function Login(){
+	const [email, setEmail] = useState<string>('');
+	const [password, setPassword] = useState<string>('');
+	const [error, setError] = useState<string>('');
+
+	const {loading, error: authError} = useAuthentication();
+
+	function handleSubmit(e: React.FormEvent<HTMLFormElement>){
+		e.preventDefault();
+
+		setError("");
+
+		const user = {
+			email,
+            password
+		}
+	}
+
 	return (
-		<div>
-			<h1>
-				Login
-			</h1>
+		<div className={styles.login}>
+			<h1>Entrar</h1>
+			<p>Faça o login para poder utilizar o sistema</p>
+			<form onSubmit={handleSubmit}>
+				<label>
+					<span>Email: </span>
+					<input type="email" name="email" required placeholder="Email do usuário" value={email} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} />
+				</label>
+				<label>
+					<span>Senha: </span>
+					<input type="password" name="password" required placeholder="Insira sua senha" value={password} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} />
+				</label>
+				{!loading && <button className="btn">Cadastrar</button>}
+				{loading && <button className="btn" disabled>Aguarde...</button>}
+				{error && <p className="error">{error}</p>}
+			</form>
 		</div>
 	)
 }
