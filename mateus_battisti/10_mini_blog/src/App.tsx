1ed6b9e16f9961a9ab6a253a4650a2d8688500b0
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-05-13 13:13:39 -0300

    Bloqueando páginas autenticadas

--- mateus_battisti/10_mini_blog/src/App.tsx
+++ mateus_battisti/10_mini_blog/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { onAuthStateChanged } from 'firebase/auth';
 import useAuthentication from './hooks/useAuthentication';
 
@@ -42,10 +42,10 @@ function App() {
             <Routes>
             <Route path='/' element={<Home/>}/>
               <Route path='/about' element={<About/>}/>
-              <Route path='/login' element={<Login/>}/>
-              <Route path='/register' element={<Register/>}/>
-              <Route path='/posts/create' element={<CreatePost/>}/>
-              <Route path='/dashboard' element={<Dashboard/>}/>
+              <Route path='/login' element={!user ? <Login/> : <Navigate to="/"/>}/>
+              <Route path='/register' element={!user ? <Register/> : <Navigate to="/"/>}/>
+              <Route path='/posts/create' element={user ? <CreatePost/> : <Navigate to="/login"/>}/>
+              <Route path='/dashboard' element={user ? <Dashboard/> : <Navigate to="/login"/>}/>
             </Routes>
           </div>
           <Footer/>
--- mateus_battisti/10_mini_blog/src/pages/About/About.tsx
+++ mateus_battisti/10_mini_blog/src/pages/About/About.tsx
@@ -1,11 +1,12 @@
+import { Link } from 'react-router-dom';
 import styles from './About.module.css';
 
 export default function About(){
 	return (
-		<div>
-			<h1>
-				About
-			</h1>
+		<div className={styles.about}>
+			<h2>Sobre o mini <span>Blog</span></h2>
+			<p>Este projeto consiste em um blog feito com React no frontend e firebase no backend</p>
+			<Link to="/posts/create" className='btn'>Criar Post</Link>
 		</div>
 	)
 }
